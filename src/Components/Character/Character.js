0221094203f45b2a411e7271535984109726ecd8
M0r0zKa <chesteroff97@gmail.com>
2023-05-23 23:54:47 +0300

    hw 1 RM

--- src/Components/Character/Character.js
+++ src/Components/Character/Character.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
 function Character({item}) {
+    //id,name,status,species,gender,image
 
     return (
         <div>
             <h3>{item.name}</h3>
-            <img src={item.img} alt={item.name}/>
-            <p>
-                <b>{item.info}</b></p>
+            <p> ID - {item.id}</p>
+            <img src={item.image} alt={item.name}/>
+            <p> Status - {item.status}</p>
+            <p> Species - {item.species}</p>
+            <p> Gender - {item.gender}</p>
+            <br/>
         </div>
     );
 }
